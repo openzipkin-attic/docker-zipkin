77bf72f1f87c71298ce6dc0e2fad3a4e2e42bbac
Adrian Cole <acole@pivotal.io>
2015-10-12 19:49:52 -0700

    Uses STORAGE_TYPE variable to decouple span storage implementations
    Users can now assign `STORAGE_TYPE` as `cassandra` or `mysql` to start
    zipkin on the corresponding backend.
    
    Fixes #56

--- collector/run.sh
+++ collector/run.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-if [[ -z $CASSANDRA_CONTACT_POINTS ]]; then
-  if [[ -z $DB_PORT_9042_TCP_ADDR ]]; then	
-  	echo "** ERROR: You need to link container with Cassandra container or specify CASSANDRA_CONTACT_POINTS env var."
-  	echo "DB_PORT_9042_TCP_ADDR (container link) or CASSANDRA_CONTACT_POINTS should contain a comma separated list of Cassandra contact points"
-  	exit 1
-  fi
-  CASSANDRA_CONTACT_POINTS=$DB_PORT_9042_TCP_ADDR
-fi
-
-export CASSANDRA_CONTACT_POINTS
-echo "Cassandra contact points: $CASSANDRA_CONTACT_POINTS"
+source .${STORAGE_TYPE}_profile
 
 echo "** Starting zipkin collector..."
-java -jar zipkin-collector.jar -f /collector-cassandra.scala
+java -jar zipkin-collector.jar -f /collector-${STORAGE_TYPE}.scala
--- query/run.sh
+++ query/run.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-if [[ -z $CASSANDRA_CONTACT_POINTS ]]; then
-  if [[ -z $DB_PORT_9042_TCP_ADDR ]]; then  
-    echo "** ERROR: You need to link container with Cassandra container or specify CASSANDRA_CONTACT_POINTS env var."
-    echo "DB_PORT_9042_TCP_ADDR (container link) or CASSANDRA_CONTACT_POINTS should contain a comma separated list of Cassandra contact points"
-    exit 1
-  fi
-  CASSANDRA_CONTACT_POINTS=$DB_PORT_9042_TCP_ADDR
-fi
-
-export CASSANDRA_CONTACT_POINTS
-echo "Cassandra contact points: $CASSANDRA_CONTACT_POINTS"
+source .${STORAGE_TYPE}_profile
 
 if [ "$COLLECTOR_PORT_9410_TCP_ADDR" ]; then
   export SCRIBE_HOST=$COLLECTOR_PORT_9410_TCP_ADDR
@@ -17,4 +7,4 @@ if [ "$COLLECTOR_PORT_9410_TCP_ADDR" ]; then
 fi
 
 echo "** Starting zipkin query..."
-java -jar zipkin-query.jar -f /query-cassandra.scala
+java -jar zipkin-query.jar -f /query-${STORAGE_TYPE}.scala
