7ac98cee411ea79b0dff622c2ea55e0e6e15ab52
Adrian Cole <adriancole@users.noreply.github.com>
2016-09-30 16:28:13 +0800

    Adds experimental support for Cassandra 3 (#111)
    This adds the schema declaration and profile needed for Cassandra 3.
    Note that Cassandra 3.9+ is needed. This means we can't use Datastax
    repository anymore as they are still at the 3.0.x level. Also, the main
    Apache repository doesn't have 3.9 either. This should wait until the
    actual dist is released.

--- cassandra/README.md
+++ cassandra/README.md
@@ -1,9 +1,10 @@
 This is temporary until we have a means to change yaml at runtime, to set the container IP, etc.
 
 ```bash
-CASSANDRA_VERSION=3.0.8
-curl -L http://downloads.datastax.com/community/dsc-cassandra-$CASSANDRA_VERSION-bin.tar.gz | tar xz
-javac -classpath dsc-cassandra-$CASSANDRA_VERSION/lib/apache-cassandra-$CASSANDRA_VERSION.jar ZipkinConfigurationLoader.java
+CASSANDRA_VERSION=3.9
+# DataStax only hosts 3.0 series at the moment
+curl -SL http://archive.apache.org/dist/cassandra/$CASSANDRA_VERSION/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz | tar xz
+javac -classpath apache-cassandra-$CASSANDRA_VERSION/lib/apache-cassandra-$CASSANDRA_VERSION.jar ZipkinConfigurationLoader.java
 git add  ZipkinConfigurationLoader.class
-rm -rf dsc-cassandra-$CASSANDRA_VERSION
+rm -rf apache-cassandra-$CASSANDRA_VERSION
 ```
--- cassandra/install.sh
+++ cassandra/install.sh
@@ -2,20 +2,17 @@
 set -eu
 
 echo "*** Installing Cassandra"
-curl -SL http://downloads.datastax.com/community/dsc-cassandra-$CASSANDRA_VERSION-bin.tar.gz | tar xz
-mv dsc-cassandra-$CASSANDRA_VERSION/* /cassandra/
+# DataStax only hosts 3.0 series at the moment
+curl -SL http://archive.apache.org/dist/cassandra/$CASSANDRA_VERSION/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz | tar xz
+mv apache-cassandra-$CASSANDRA_VERSION/* /cassandra/
 
 echo "*** Installing Python"
 apk add --update --no-cache python
 
-# Add patch files needed to work around python incompatibility
-rm /cassandra/lib/cassandra-driver-internal-only-*
-cp -r /cassandra/CASSANDRA-11850/* /cassandra/
-
 # TODO: Add native snappy lib. Native loader stacktraces in the cassandra log as a results, which is distracting.
 
 echo "*** Starting Cassandra"
-/cassandra/bin/cassandra
+/cassandra/bin/cassandra -R
 
 timeout=300
 while [[ "$timeout" -gt 0 ]] && ! /cassandra/bin/cqlsh -e 'SHOW VERSION' localhost >/dev/null 2>/dev/null; do
@@ -28,11 +25,18 @@ echo "*** Importing Scheme"
 curl https://raw.githubusercontent.com/openzipkin/zipkin/$ZIPKIN_VERSION/zipkin-storage/cassandra/src/main/resources/cassandra-schema-cql3.txt \
      | /cassandra/bin/cqlsh --debug localhost
 
+curl https://raw.githubusercontent.com/openzipkin/zipkin/$ZIPKIN_VERSION/zipkin-storage/cassandra3/src/main/resources/cassandra3-schema.cql \
+     | /cassandra/bin/cqlsh --debug localhost
+
 echo "*** Stopping Cassandra"
 pkill -f java
 
 echo "*** Cleaning Up"
 apk del python --purge
 rm -rf /cassandra/javadoc/ /cassandra/pylib/ /cassandra/tools/ /cassandra/lib/*.zip
+
+echo "*** Changing to cassandra user"
+adduser -S cassandra
+chown -R cassandra /cassandra
 
 echo "*** Image build complete"
