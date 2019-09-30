f16c4e29e64e4644bec532cbb5bb694697ab180c
Brian Devins-Suresh <badevins@gmail.com>
2019-09-30 09:44:34 -0400

    Allow sending arguments to spring

--- zipkin/zipkin/run.sh
+++ zipkin/zipkin/run.sh
@@ -4,4 +4,4 @@ if [ -f ".${STORAGE_TYPE}_profile" ]; then
   source ${PWD}/.${STORAGE_TYPE}_profile
 fi
 
-exec java ${MODULE_OPTS} ${JAVA_OPTS} -cp . org.springframework.boot.loader.PropertiesLauncher
+exec java ${MODULE_OPTS} ${JAVA_OPTS} -cp . org.springframework.boot.loader.PropertiesLauncher "$@"
