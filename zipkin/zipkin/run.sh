a3be779e5ab136dbba6725a9228dc5bbc5e99fb7
Adrian Cole <adriancole@users.noreply.github.com>
2019-10-15 15:20:27 +0800

    Switches to launch with main class vs PropertiesLauncher when possible (#229)
    After researching before and after startup times with Spring Boot 2.1
    vs 2.2, the largest win for startup time, regardless of version, is to
    not use `PropertiesLauncher`.

--- zipkin/zipkin/run.sh
+++ zipkin/zipkin/run.sh
@@ -4,4 +4,10 @@ if [ -f ".${STORAGE_TYPE}_profile" ]; then
   source ${PWD}/.${STORAGE_TYPE}_profile
 fi
 
-exec java ${MODULE_OPTS} ${JAVA_OPTS} -cp . org.springframework.boot.loader.PropertiesLauncher
+# Use main class directly if there are no modules, as it measured 14% faster from JVM running to available
+# verses PropertiesLauncher when using Zipkin was based on Spring Boot 2.1
+if [[ -z "$MODULE_OPTS" ]]; then
+  exec java ${JAVA_OPTS} -cp .:$(ls BOOT-INF/lib/*.jar|tr '\n' ':')BOOT-INF/classes zipkin.server.ZipkinServer
+else
+  exec java ${MODULE_OPTS} ${JAVA_OPTS} -cp . org.springframework.boot.loader.PropertiesLauncher
+fi
