2de919adad712f82e96d166b9e84a23858c53d4b
Adrian Cole <acole@pivotal.io>
2019-10-17 09:55:44 +0800

    statically evaluate classpath to avoid problem in docker for mac
    Under certain circumstances, the `ls` command can't be invoked in mac.
    This avoids it by moving the `ls` to build time.
    
    See https://github.com/docker/for-mac/issues/3643
    Fixes #232

--- zipkin-slim/zipkin/run.sh
+++ zipkin-slim/zipkin/run.sh
@@ -4,4 +4,4 @@ if [ -f ".${STORAGE_TYPE}_profile" ]; then
   source ${PWD}/.${STORAGE_TYPE}_profile
 fi
 
-exec java ${JAVA_OPTS} -cp .:$(ls ${PWD}/BOOT-INF/lib/*.jar|tr '\n' ':')${PWD}/BOOT-INF/classes zipkin.server.ZipkinServer
+exec java ${JAVA_OPTS} -cp $(cat /zipkin/classpath) zipkin.server.ZipkinServer
